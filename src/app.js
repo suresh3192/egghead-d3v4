dc0af21ee069a35a587828b8425987ea54c29d18
suresh <suresh3192@gmail.com>
2016-11-06 13:09:58 -0500

    Example for Timescale

--- src/app.js
+++ src/app.js
@@ -8,10 +8,18 @@
 
 
 var timeScale = d3.scaleTime()
-  .domain([new Date(2016, 0, 1), new Date()])
-  .range([0, 100]);
+  .domain([new Date(2016, 0, 1), new Date(2016,11,31)])
+  .range([1, 365]);
 
-console.log(timeScale(new Date(2016, 0, 15)));
+
+var today = new Date();
+var timeScale1 = d3.scaleTime().domain([today,new Date(today.getFullYear()+1,today.getMonth(),today.getDay())]).range([0,100]);
+
+
+console.log(timeScale1.invert(300));
+
+
+console.log(timeScale(new Date()));
 console.log(timeScale(new Date(2016, 3, 15)));
 console.log(timeScale(new Date()));
 
