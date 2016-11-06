4bc95b3ce578b4c547316963c3b61ec4bfde58a0
suresh <suresh3192@gmail.com>
2016-11-06 15:20:07 -0500

    dom manipulation

--- src/app.js
+++ src/app.js
@@ -5,3 +5,6 @@ d3.select('.title')
   .append('button')
     .style('display', 'block')
     .text('submit');
+d3.select('body')
+    .insert('h1','.title')
+        .text('D3.js DOM element creation and insertion');
